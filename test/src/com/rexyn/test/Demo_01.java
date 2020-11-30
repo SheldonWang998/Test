1e1bc0584aefcb95444e264d9cd096829c754892
SheldonWang998 <1053062318@qq.com>
2020-11-30 16:35:53 +0800

    frist

---
+++ test/src/com/rexyn/test/Demo_01.java
@@ -0,0 +1,13 @@
+package com.rexyn.test;
+
+/**
+ * @ClassName: Demo_01
+ * @Author: sheldon
+ * @Date: 2020/11/30
+ * @Description:
+ */
+public class Demo_01 {
+    public static void main(String[] args) {
+        System.out.println("hello world!!");
+    }
+}
