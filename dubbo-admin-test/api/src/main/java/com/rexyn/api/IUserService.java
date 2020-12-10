1cb580c652a17aa0c8ae4c961ee856e904767b3a
SheldonWang998 <1053062318@qq.com>
2020-12-10 17:40:34 +0800

    springboot + dubbo

---
+++ dubbo-admin-test/api/src/main/java/com/rexyn/api/IUserService.java
@@ -0,0 +1,11 @@
+package com.rexyn.api;
+
+/**
+ * @ClassName: IUser
+ * @Author: sheldon
+ * @Date: 2020/12/10
+ * @Description:
+ */
+public interface IUserService {
+    String sayHello(String username);
+}
---
+++ dubbo-admin-test/provider/src/main/java/com/example/provider/user/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.example.provider.user;
+
+import com.rexyn.api.IUserService;
+
+/**
+ * @ClassName: UserServiceImpl
+ * @Author: sheldon
+ * @Date: 2020/12/10
+ * @Description:
+ */
+public class UserServiceImpl implements IUserService {
+    @Override
+    public String sayHello(String username) {
+        return null;
+    }
+}
