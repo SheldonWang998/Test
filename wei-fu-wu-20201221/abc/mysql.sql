180848604dc601ce5d01f9d9a2b8f9430c1159a6
SheldonWang998 <1053062318@qq.com>
2020-12-30 14:45:52 +0800

    20201221

---
+++ wei-fu-wu-20201221/abc/mysql.sql
@@ -0,0 +1,7 @@
+delete from credit_record_ent where city_no = '500000' and  ztbh='QY50000020201222000002880';
+delete from credit_record_ent where city_no = '500000' and  ztbh='QY50000020200914000001592';
+delete from credit_record_ent where city_no = '500000' and  ztbh='QY50000020201224000003890';
+delete from credit_record_ent where city_no = '500000' and  ztbh='QY50000020201117000001812';
+delete from credit_record_ent where city_no = '500000' and  ztbh='QY50000020201214000002324';
+delete from credit_record_ent where city_no = '500000' and  ztbh='QY50000020201216000002362';
+commit;
