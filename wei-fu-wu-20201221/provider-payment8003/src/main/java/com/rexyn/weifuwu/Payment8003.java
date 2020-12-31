9f171b4a0ac5bc185385213412a47b0ebfec977e
SheldonWang998 <1053062318@qq.com>
2020-12-31 10:38:34 +0800

    20201231

---
+++ wei-fu-wu-20201221/provider-payment8003/src/main/java/com/rexyn/weifuwu/Payment8003.java
@@ -0,0 +1,7 @@
+package com.rexyn.weifuwu;/**
+   *创建时间：2020/12/31 10:07
+   *创建人：sheldon
+   *描述：
+   */
+public class Payment8003 {
+}
---
+++ wei-fu-wu-20201221/provider-payment8003/src/main/java/com/rexyn/weifuwu/controller/PaymentController.java
@@ -0,0 +1,7 @@
+package com.rexyn.weifuwu.controller;/**
+   *创建时间：2020/12/31 10:10
+   *创建人：sheldon
+   *描述：
+   */
+public class PaymentController {
+}
