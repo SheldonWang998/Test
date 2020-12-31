f5b91a00437b4388d87790b2fd1f0fda04e298d7
SheldonWang998 <1053062318@qq.com>
2020-12-31 10:41:30 +0800

    122131

--- wei-fu-wu-20201221/provider-payment8003/src/main/java/com/rexyn/weifuwu/Payment8003.java
+++ wei-fu-wu-20201221/provider-payment8003/src/main/java/com/rexyn/weifuwu/Payment8003.java
@@ -1,7 +1,19 @@
-package com.rexyn.weifuwu;/**
-   *创建时间：2020/12/31 10:07
-   *创建人：sheldon
-   *描述：
-   */
+package com.rexyn.weifuwu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 创建时间：2020/12/31 10:07
+ * 创建人：sheldon
+ * 描述：
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
 public class Payment8003 {
+    public static void main(String[] args) {
+        SpringApplication.run(Payment8003.class,args);
+    }
+
 }
--- wei-fu-wu-20201221/provider-payment8003/src/main/java/com/rexyn/weifuwu/controller/PaymentController.java
+++ wei-fu-wu-20201221/provider-payment8003/src/main/java/com/rexyn/weifuwu/controller/PaymentController.java
@@ -1,7 +1,26 @@
-package com.rexyn.weifuwu.controller;/**
-   *创建时间：2020/12/31 10:10
-   *创建人：sheldon
-   *描述：
-   */
+package com.rexyn.weifuwu.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * 创建时间：2020/12/31 10:10
+ * 创建人：sheldon
+ * 描述：
+ */
+@RestController
+@Slf4j
 public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/payment/zk")
+    public String paymentForZK() {
+        return "spring cloud with zookeeper" + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+
 }
