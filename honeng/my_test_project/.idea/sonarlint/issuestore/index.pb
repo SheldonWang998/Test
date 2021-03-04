e111354d9b6d00b25191614c82e2fdcdaf53938b
SheldonWang998 <1053062318@qq.com>
2021-03-04 14:27:48 +0800

    111

--- honeng/my_test_project/.idea/sonarlint/issuestore/index.pb
+++ honeng/my_test_project/.idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,7 @@ f
 6src/main/java/com/rexyn/controller/UserController.java,7\2\72c5313aff10e40996ea34e823560b63781f15e8
 X
 (src/main/java/com/rexyn/Application.java,9\a\9aef63846d2260497154a7043f4ed3a306d21c23
+=
+src/test.text,f\c\fc52e7642645838d10b574c30d3d6f2a99d1b422
+^
+.src/main/java/com/rexyn/service/impl/Test.java,9\e\9e6f4ad4a5316209f11190b0d5a1098417ace358
---
+++ honeng/my_test_project/src/main/java/com/rexyn/service/impl/Test.java
@@ -0,0 +1,45 @@
+package com.rexyn.service.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+
+/**
+ * @author sheldon
+ * 描述：
+ * @date 2021/1/22 10:34
+ */
+public class Test {
+    private static final Logger logger = LoggerFactory.getLogger("Test");
+
+    /**
+     * 输入流：将外界信息输入到系统
+     * 输出流：将系统信息输出到外界
+     *
+     * @param args 参数
+     */
+    public static void main(String[] args) throws Exception {
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream("src" + File.separator + "test.text");
+            byte[] b = new byte[1024];
+            logger.info("开始读取文件");
+            int len = 1;
+            while ((len = fis.read(b)) != -1) {
+                String str = new String(b, "UTF-8");
+                logger.info(str);
+            }
+            logger.info("文件读取完毕！");
+        } catch (Exception e) {
+            logger.info("文件读取异常");
+            e.printStackTrace();
+        } finally {
+            assert fis != null;
+            fis.close();
+        }
+
+    }
+}
